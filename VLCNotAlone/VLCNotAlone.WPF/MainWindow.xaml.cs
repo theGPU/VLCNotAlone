b7da99b3f102d231043a99f3bca0ef58fbf2d473
titaniumX712 <42218370+titaniumX712@users.noreply.github.com>
2023-05-11 14:25:06 +0300

    Rewrite base init

---
+++ VLCNotAlone/VLCNotAlone.WPF/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using LibVLCSharp.Forms.Platforms.WPF;
+using LibVLCSharp.Forms.Shared;
+using System.Collections.Generic;
+using System.Reflection;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.WPF;
+
+namespace VLCNotAlone.WPF
+{
+    public partial class MainWindow : FormsApplicationPage
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitDependencies();
+            Forms.Init();
+            LoadApplication(new VLCNotAlone.App());
+        }
+
+        void InitDependencies()
+        {
+            var init = new List<Assembly>
+            {
+                typeof(VideoView).Assembly,
+                typeof(VideoViewRenderer).Assembly
+            };
+        }
+    }
+}
---
+++ VLCNotAlone/VLCNotAlone/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using LibVLCSharp.Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace VLCNotAlone
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            ((MainViewModel)BindingContext).OnAppearing();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            ((MainViewModel)BindingContext).OnDisappearing();
+        }
+
+        private void VideoView_MediaPlayerChanged(object sender, MediaPlayerChangedEventArgs e)
+        {
+            ((MainViewModel)BindingContext).OnVideoViewInitialized();
+        }
+    }
+}
