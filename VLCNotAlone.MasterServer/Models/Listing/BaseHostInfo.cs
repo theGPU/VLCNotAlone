7e13a991623f3fb5e344074687615c4599ab3f2e
titaniumX712 <42218370+titaniumX712@users.noreply.github.com>
2023-05-12 02:31:26 +0300

    MasterServer checkpoint

--- VLCNotAlone.MasterServer/Models/Listing/BaseHostInfo.cs
+++ VLCNotAlone.MasterServer/Models/Listing/BaseHostInfo.cs
@@ -1,15 +1,22 @@
-﻿namespace VLCNotAlone.MasterServer.Models.Listing
+﻿using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+
+namespace VLCNotAlone.MasterServer.Models.Listing
 {
     public class BaseHostInfo
     {
         public Guid ID { get; set; }
-        public string Name { get; set; }
+        [StringLength(128)] public string Name { get; set; }
+        [StringLength(4096)] public string Description { get; set; }
 
         public ushort Port { get; set; }
 
         public bool IsPublic { get; set; }
+        [StringLength(256)] public string? Password { get; set; }
         public int ProtocolVersion { get; set; }
         public int ClientsCount { get; set; }
         public int RoomsCount { get; set; }
+
+        public bool HasPassword => string.IsNullOrEmpty(Password);
     }
 }
